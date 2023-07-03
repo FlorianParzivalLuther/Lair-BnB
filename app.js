0a29dd9b9b191182902fc5f1eeaaecef91a6f237
Roman Kuptsov <romankuptsov@icloud.com>
2023-07-03 17:28:10 +0200

    added the registration for the host

--- app.js
+++ app.js
@@ -42,6 +42,7 @@ app.use("/", reviewRoutes);
 const userRoutes = require("./routes/user.routes");
 app.use("/", userRoutes);
 
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
 
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -34,4 +34,34 @@ router.get("/user", (req, res) => {
   res.render("user", { userInSession: req.session.currentUser });
 });
 
+// BECOMING A HOST //
+
+router.get("/becomeHost", (req, res) => {
+  res.render("register");
+});
+
+// Register is defined in the controlers/auth.js
+router.post("/becomeHost", register);
+
+// Render Login Page for users
+router.get("/becomeHost", (req, res) => {
+  res.render("becomeHost");
+});
+
+// Login
+router.post("/becomeHost", login);
+
+// Logout
+router.post("/logout", (req, res, next) => {
+  req.session.destroy((err) => {
+    if (err) next(err);
+    res.redirect("/");
+  });
+});
+
+// User Profile Page
+router.get("/host", (req, res) => {
+  res.render("host", { userInSession: req.session.currentUser });
+});
+
 module.exports = router;
