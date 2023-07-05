c1d41418e11d48f4242a2da12ae824bc8dbe9661
Florian Luther <hi@gauvainstudios.com>
2023-07-05 09:39:06 +0200

    last changes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,60 @@
+// const express = require("express");
+// const router = express.Router();
+
+// const User = require("../models/User.model");
+
+
+// router.get("/user/:userId", async (req, res) => {
+//   // Get a specific user by ID
+//   try {
+//     const user = await User.findById(req.params.userId);
+//     if (!user) {
+//       return res.status(404).json({ error: "User not found" });
+//     }
+//     res.json(user);
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+
+// // Rendering example
+// router.get("/user", async (req, res) => {
+//   try {
+//     const users = await User.find();
+//     res.render("user", { users });
+//   } catch (error) {
+//     res.status(500).json({ error: "Internal server error" });
+//   }
+// });
+
+// module.exports = router;
+
+
+
 const express = require("express");
 const router = express.Router();
 
 const User = require("../models/User.model");
 
+router.get("/user", async (req, res) => {
+  try {
+    const currentUser = req.session.currentUser;
+    console.log(currentUser);
+    if (currentUser) {
+      const user = await User.findById(currentUser.id);
+      if (user) {
+        const users = await User.find();
+        res.render("user", { users });
+      } else {
+        res.redirect("/loginUser");
+      }
+    } else {
+      res.redirect("/user");
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 router.get("/user/:userId", async (req, res) => {
   // Get a specific user by ID
@@ -12,16 +64,6 @@ router.get("/user/:userId", async (req, res) => {
       return res.status(404).json({ error: "User not found" });
     }
     res.json(user);
-  } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-// Rendering example
-router.get("/user", async (req, res) => {
-  try {
-    const users = await User.find();
-    res.render("user", { users });
   } catch (error) {
     res.status(500).json({ error: "Internal server error" });
   }
