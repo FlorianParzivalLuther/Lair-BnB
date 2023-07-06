22972a520981ebc150e2dc9404ac80c2122aec62
Filipa Barros <barrosfilipa.f@gmail.com>
2023-07-06 18:38:59 +0200

    user edit stuff

--- routes/host.routes.js
+++ routes/host.routes.js
@@ -2,21 +2,64 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User.model");
 const Host = require("../models/Host.model");
+const Property = require("../models/Property.model");
+
+// router.get("/host", async (req, res) => {
+//   const { currentHost, currentUser } = req.session;
+//   console.log(currentHost);
+
+//   const hostId = currentHost.id;
+
+//   if (currentUser) {
+//     res.redirect("/user");
+//     return;
+//   } else {
+//     if (currentHost) {
+//       Property.find({ owner: hostId })
+//         .populate("owner") // Populate the 'ownerId' field
+//         .then((properties) => {
+//           res.render("properties", { properties });
+//         })
+//         .catch((error) => {
+//           console.error(error);
+//         });
+
+//       res.render("host", { hostInSession: currentHost });
+//       return;
+//     } else {
+//       res.redirect("/loginHost");
+//       return;
+//     }
+//   }
+// });
 
 router.get("/host", async (req, res) => {
   const { currentHost, currentUser } = req.session;
+  console.log(currentHost);
+
+  const hostId = currentHost._id;
+
   if (currentUser) {
     res.redirect("/user");
     return;
-  } else {
-    if (currentHost) {
-      res.render("host", { hostInSession: currentHost });
-      return;
-    } else {
-      res.redirect("/loginHost");
-      return;
-    }
   }
+
+  if (currentHost) {
+    Property.find({ owner: hostId })
+      // .populate("createdProperties")
+      .then((properties) => {
+        res.render("host", { hostInSession: currentHost, properties });
+        console.log(properties);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.redirect("/error"); // Handle any error
+      });
+
+    return;
+  }
+
+  res.redirect("/loginHost");
 });
 
 router.get("/host/:hostId", async (req, res) => {
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -20,8 +20,41 @@ router.get("/user", async (req, res) => {
   }
 });
 
+// Get edit page
+router.get("/user/:userId/edit", (req, res) => {
+  User.findById(req.params.userId)
+    .then((user) => {
+      if (user) {
+        console.log(user);
+        res.render("user-edit");
+      } else {
+        return res.status(404).json({ error: "user not found" });
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: "Internal server error" });
+    });
+});
 
+// Post edited user
 
+router.post("/user/:userId/edit", async (req, res) => {
+  console.log("booooooddddddyyyyyyy", req.body);
+  console.log(req.params);
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.userId,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    // console.log(updatedProperty);
+    res.status(200).redirect(`/user`);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get("/user/:userId", async (req, res) => {
   // Get a specific user by ID
