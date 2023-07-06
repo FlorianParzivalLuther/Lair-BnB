eb444715e9cb56c21d9d02e2ad73585ccc473f85
Florian Luther <hi@gauvainstudios.com>
2023-07-06 20:18:29 +0200

    -.-

--- db/index.js
+++ db/index.js
@@ -1,20 +1,20 @@
-// ℹ️ package responsible to make the connection with mongodb
-// https://www.npmjs.com/package/mongoose
-const mongoose = require("mongoose");
-require("dotenv").config();
-// ℹ️ Sets the MongoDB URI for our app to have access to it.
-// If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
+// // ℹ️ package responsible to make the connection with mongodb
+// // https://www.npmjs.com/package/mongoose
+// const mongoose = require("mongoose");
+// require("dotenv").config();
+// // ℹ️ Sets the MongoDB URI for our app to have access to it.
+// // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || process.env.DATABASE;
-mongoose
-  .connect(MONGO_URI)
-  .then((x) => {
-    const databaseName = x.connections[0].name;
-    console.log(`Connected to Mongo! Database name: "${databaseName}"`);
-  })
-  .catch((err) => {
-    console.error("Error connecting to mongo: ", err);
-  });
+// const MONGO_URI = process.env.MONGODB_URI || process.env.DATABASE;
+// mongoose
+//   .connect(MONGO_URI)
+//   .then((x) => {
+//     const databaseName = x.connections[0].name;
+//     console.log(`Connected to Mongo! Database name: "${databaseName}"`);
+//   })
+//   .catch((err) => {
+//     console.error("Error connecting to mongo: ", err);
+//   });
 
-//Preset MongoDB Local Database
-//"mongodb://127.0.0.1:27017/project-2";
+// //Preset MongoDB Local Database
+// //"mongodb://127.0.0.1:27017/project-2";
