86d2f0a87e74a08d7ba10a03b310ed8f995794a2
Filipa Barros <barrosfilipa.f@gmail.com>
2023-07-06 09:59:41 +0200

    checkout function

--- controllers/checkout.js
+++ controllers/checkout.js
@@ -1,10 +1,19 @@
-const Booking = required("../models/Booking.model");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const Booking = require("../models/Booking.model");
 
-const getCheckoutSession = async(req, res, next) => {
+const getCheckoutSession = async (req, res, next) => {
+  const booking = await Booking.findById(req.params.bookingId);
 
-    const booking = await Booking.findById(req.params.bookingId);
+  stripe.checkout.session.create({
+    payment_method_types: ["card"],
+    success_url: `${req.protocol}://${req.get("host")}/`,
+    cancel_url: `${req.protocol}://${req.get("host")}`,
+    customer_email: req.user.email, // find user email --> probably need to change this
+    client_reference_id: req.params.bookingId,
+    //     line_items: [
+    //         {
+    // name: `${}`
 
-
-
-    
-}
+    //         }
+  });
+};
