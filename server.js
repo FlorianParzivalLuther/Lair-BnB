60244edb2a2740b388a0a57cb4a0777204216b00
Florian Luther <hi@gauvainstudios.com>
2023-07-06 19:55:35 +0200

    done

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-const app = require("./app");
-
+const mongoose = require("mongoose");
+require("dotenv").config();
 // ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 3000
 const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGODB_URI || process.env.DATABASE;
 
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-});
+const connectDB = async () => {
+  try {
+    const response = await mongoose.connect(MONGO_URI);
+    console.log("Atlas connected to db:", response.connections[0].name);
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    process.exit(1);
+  }
+};
+connectDB().then(runServer);
+
+const app = require("./app");
+
+function runServer() {
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
+}
